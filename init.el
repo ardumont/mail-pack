193f4d66151e9b2110a33335caddc4b98b31e415
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-30 11:31:11 +0200

    Load the mail-pack if the setup is possible. If not, provide an enlightning help on howto do it.

--- init.el
+++ init.el
@@ -1,51 +1,79 @@
 (require 'gnus)
 (require 'creds)
 
-;; adding the creds.el lib
-(live-add-pack-lib "creds.el")
+;; ===================== setup file
 
-;; got this line from one of the tutorials. Seemed interesting enough
-(setq gnus-invalid-group-regexp "[:`'\"]\\|^$")
+(setq mail-pack--credentials-file "~/.authinfo")
 
-;; standard way of getting imap going
-(setq gnus-select-method
-         '(nnimap "gmail"
-          (nnimap-address "imap.gmail.com")
-          (nnimap-server-port 993)
-          (nnimap-stream ssl)))
+;; ===================== setup function
 
-;; set up smtp so we can send from gmail too:
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials "~/.authinfo"
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587)
+(defun mail-pack--setup-possible-p (creds-file)
+  "Check if the setup is possible by checking the existence of the file creds-file and that the entry 'description' is provided."
+  (if (file-exists-p creds-file)
+      (let ((parsed-lines (read-lines creds-file)))
+        ;; load the entry imap.gmail.com in the ~/.netrc, we obtain a hash-map with the needed data
+        (and (get-creds parsed-lines "imap.gmail.com")
+             (get-creds parsed-lines "smtp.gmail.com")
+             (get-creds parsed-lines "description")))))
 
-;;http://www.emacswiki.org/cgi-bin/wiki/GnusGmail
-;;http://linil.wordpress.com/2008/01/18/gnus-gmail/
+(defun mail-pack--setup (creds-file)
+  ;; adding the creds.el lib
+  (live-add-pack-lib "creds.el")
 
-(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+  ;; got this line from one of the tutorials. Seemed interesting enough
+  (setq gnus-invalid-group-regexp "[:`'\"]\\|^$")
 
-;; Threads are nice!
-(setq gnus-summary-thread-gathering-function
-      'gnus-gather-threads-by-subject)
+  ;; standard way of getting imap going
+  (setq gnus-select-method
+        '(nnimap "gmail"
+                 (nnimap-address "imap.gmail.com")
+                 (nnimap-server-port 993)
+                 (nnimap-stream ssl)))
 
-;; load the entry imap.gmail.com in the ~/.netrc, we obtain a hash-map with the needed data
-(setq description (get-creds (read-lines "~/.authinfo") "description"))
+  ;; set up smtp so we can send from gmail too:
+  (setq message-send-mail-function 'smtpmail-send-it
+        smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+        smtpmail-auth-credentials creds-file
+        smtpmail-default-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 587)
 
-(setq full-name (concat (get-entry description "firstname") " "
-                        (get-entry description "surname") " "
-                        (get-entry description "name")))
+  ;;http://www.emacswiki.org/cgi-bin/wiki/GnusGmail
+  ;;http://linil.wordpress.com/2008/01/18/gnus-gmail/
 
-(setq x-url (get-entry description "x-url"))
-(setq mail-address (get-entry description "mail"))
-(setq mail-host (get-entry description "mail-host"))
+  (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
-(setq gnus-posting-styles
-       '((".*"
-          (name full-name)
-          ("X-URL" x-url)
-          (mail-host-address mail-host))))
+  ;; Threads are nice!
+  (setq gnus-summary-thread-gathering-function
+        'gnus-gather-threads-by-subject)
 
-(setq send-mail-function 'smtpmail-send-it)
+  (setq description (get-creds (read-lines creds-file) "description"))
+
+  (setq full-name (concat (get-entry description "firstname") " "
+                          (get-entry description "surname") " "
+                          (get-entry description "name")))
+
+  (setq x-url (get-entry description "x-url"))
+  (setq mail-address (get-entry description "mail"))
+  (setq mail-host (get-entry description "mail-host"))
+
+  (setq gnus-posting-styles
+        '((".*"
+           (name full-name)
+           ("X-URL" x-url)
+           (mail-host-address mail-host))))
+
+  (setq send-mail-function 'smtpmail-send-it))
+
+;; ===================== setup routine
+
+(if (mail-pack--setup-possible-p mail-pack--credentials-file)
+    (progn
+      (message (concat mail-pack--credentials-file " found! Running Setup..."))
+      (mail-pack--setup mail-pack--credentials-file)
+      (message "Setup done!"))
+  (message (concat "You need to setup the credentials file " mail-pack--credentials-file " for this to work.\n"
+                   "Here is the needed content to setup to your need into '" mail-pack--credentials-file "':\n"
+                   "machine imap.gmail.com login <your-email> password <your-mail-password-or-dedicated-passwd> port 993\n"
+                   "machine smtp.gmail.com login <login> port 587 password <your-mail-password-or-dedicated-passwd>\n"
+                   "machine description firstname <firstname> surname <surname> name <name> x-url <some-url> mail <your-email> mail-host <your-mail-host>\n")))
