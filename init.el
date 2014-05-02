672e1e95aad87fe3ebb37b2ac723a14a3aeb666d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-02 10:29:01 +0200

    Use .authinfo.gpg file to secure the authentication tokens needed for mail access

--- init.el
+++ init.el
@@ -8,10 +8,12 @@
 
 (require 'gnus)
 (require 'creds)
+(require 'dash)
 
 ;; ===================== setup file
 
-(defvar *MAIL-PACK-CREDENTIALS-FILE* "~/.authinfo")
+;; create your .authinfo file and and encrypt it in ~/.authinfo.gpg with M-x epa-encrypt-file
+(defvar *MAIL-PACK-CREDENTIALS-FILE* "~/.authinfo.gpg")
 
 ;; ===================== setup function
 
@@ -21,11 +23,12 @@
 (defun mail-pack/setup-possible-p (creds-file) "Check if the setup is possible by checking the existence of the file creds-file and that the entries 'imap.gmail.com', 'smtp.gmail.com', and 'description' are provided."
   (if (file-exists-p creds-file)
       (let ((parsed-lines (creds/read-lines creds-file)))
-        (and (creds/get parsed-lines "imap.gmail.com")
-             (creds/get parsed-lines "smtp.gmail.com")
-             (creds/get parsed-lines "description")))))
+        (when (and (creds/get parsed-lines "imap.gmail.com")
+                   (creds/get parsed-lines "smtp.gmail.com")
+                   (creds/get parsed-lines "description"))
+          parsed-lines))))
 
-(defun mail-pack/setup (creds-file)
+(defun mail-pack/setup (creds-file creds-file-content)
   "My default gnus setup."
   ;; got this line from one of the tutorials. Seemed interesting enough
   (setq gnus-invalid-group-regexp "[:`'\"]\\|^$")
@@ -41,7 +44,7 @@
   ;; set up smtp so we can send from gmail too:
   (setq message-send-mail-function    'smtpmail-send-it
         smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-        smtpmail-auth-credentials     creds-file
+        smtpmail-auth-credentials     (expand-file-name creds-file)
         smtpmail-default-smtp-server  "smtp.gmail.com"
         smtpmail-smtp-server          "smtp.gmail.com"
         smtpmail-smtp-service         587)
@@ -54,7 +57,7 @@
   ;; Threads are nice!
   (setq gnus-summary-thread-gathering-function 'gnus-gather-threads-by-subject)
 
-  (setq description (creds/get (creds/read-lines creds-file) "description"))
+  (setq description (creds/get creds-file-content "description"))
 
   (setq full-name (format "%s %s %s" (creds/get-entry description "firstname") (creds/get-entry description "surname") (creds/get-entry description "name")))
 
@@ -74,15 +77,16 @@
 
 ;; ===================== setup routine
 
-(if (mail-pack/setup-possible-p *MAIL-PACK-CREDENTIALS-FILE*)
+(-if-let (creds-file-content (mail-pack/setup-possible-p *MAIL-PACK-CREDENTIALS-FILE*))
     (progn
       (mail-pack/log (concat *MAIL-PACK-CREDENTIALS-FILE* " found! Running Setup..."))
-      (mail-pack/setup *MAIL-PACK-CREDENTIALS-FILE*)
+      (mail-pack/setup *MAIL-PACK-CREDENTIALS-FILE* creds-file-content)
       (mail-pack/log "Setup done!"))
   (mail-pack/log (concat "You need to setup the credentials file " *MAIL-PACK-CREDENTIALS-FILE* " for this to work.\n"
                    "Here is a sample content to setup to your need into '" *MAIL-PACK-CREDENTIALS-FILE* "':\n"
                    "machine imap.gmail.com login <your-email> password <your-mail-password-or-dedicated-passwd> port 993\n"
                    "machine smtp.gmail.com login <login> port 587 password <your-mail-password-or-dedicated-passwd>\n"
-                   "machine description firstname <firstname> surname <surname> name <name> x-url <some-url> mail <your-email> mail-host <your-mail-host>\n")))
+                   "machine description firstname <firstname> surname <surname> name <name> x-url <some-url> mail <your-email> mail-host <your-mail-host>\n"
+                   "Then `M-x encrypt-epa-file` to generate the required ~/.authinfo.gpg and remove ~/.authinfo")))
 
 ;;; mail-pack.el ends here
