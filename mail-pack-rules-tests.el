411f0b39b0562499f5b1ccce8c0712c3845609c6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-08 20:24:31 +0200

    Add mail-pack-rules

---
+++ mail-pack-rules-tests.el
@@ -0,0 +1,222 @@
+;;; mail-pack-rules-tests.el ---                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Antoine Romain Dumont
+
+;; Author: Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'ert)
+
+(ert-deftest test-mail-pack-rules-filter-expand-rule--from ()
+  (should (string= "/somewhere"
+                   (let ((test-msg '(:subject "some subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule--from '(:from "foobar@some-news.com" :dest "/somewhere")) (list test-msg)))))
+  (should-not (let ((test-msg '(:subject "some super uber top mega cool subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--from '(:from "barfoo@some-news.com" :dest "/elsewhere")) (list test-msg))))
+  (should-not (let ((test-msg '(:subject "some other dumbass subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--from nil) (list test-msg)))))
+
+(ert-deftest test-mail-pack-rules-filter-expand-rule--subject ()
+  (should (string= "/somewhere"
+                   (let ((test-msg '(:subject "some [hello] subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule--subject '(:subject "\\[hello\\]"
+                                                                                    :dest "/somewhere")) (list test-msg)))))
+  (should-not (let ((test-msg '(:subject "some super uber top mega cool subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--subject '(:subject "\\[hello\\]"
+                                                                               :dest "/somewhere")) (list test-msg))))
+  (should-not (let ((test-msg '(:subject "some other dumbass subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--subject nil) (list test-msg)))))
+
+(ert-deftest test-mail-pack-rules-filter-expand-rule--to ()
+  (should (string= "/somewhere"
+                   (let ((test-msg '(:subject "some subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule--to '(:to "some@mail.com" :dest "/somewhere")) (list test-msg)))))
+  (should-not (let ((test-msg '(:subject "some super uber top mega cool subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--to '(:to "someone@else.fr" :dest "/elsewhere")) (list test-msg))))
+  (should-not (let ((test-msg '(:subject "some other dumbass subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--to nil) (list test-msg)))))
+
+(ert-deftest test-mail-pack-rules-filter-expand-rule--from-subject ()
+  (should (string= "/somewhere"
+                   (let ((test-msg '(:subject "[BLA] some hype subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule--from-subject
+                             '(:from "foobar@some-news.com"
+                                     :subject "\\[BLA\\]"
+                                     :dest "/somewhere")) (list test-msg)))))
+  (should-not (let ((test-msg '(:subject "some super uber top mega cool subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--from-subject
+                        '(:from "foobar@some-news.com"
+                                :subject "\\[BLA\\]"
+                                :dest "/somewhere")) (list test-msg))))
+  (should-not (let ((test-msg '(:subject "[BLA] some super uber top mega cool subject"
+                                         :from (("someone" . "someother@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--from-subject
+                        '(:from "foobar@some-news.com"
+                                :subject "\[BLA\]"
+                                :dest "/somewhere")) (list test-msg))))
+  (should-not (let ((test-msg '(:subject "some other dumbass subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--from-subject nil) (list test-msg)))))
+
+(ert-deftest test-mail-pack-rules-filter-expand-rule--to-subject ()
+  (should (string= "/somewhere"
+                   (let ((test-msg '(:subject "[BLA] some hype subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule--to-subject
+                             '(:to "some@mail.com"
+                                   :subject "\\[BLA\\]"
+                                   :dest "/somewhere")) (list test-msg)))))
+  (should-not (let ((test-msg '(:subject "some super uber top mega cool subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--to-subject
+                        '(:to "some@mail.com"
+                              :subject "\\[BLA\\]"
+                              :dest "/somewhere")) (list test-msg))))
+  (should-not (let ((test-msg '(:subject "[BLA] some super uber top mega cool subject"
+                                         :from (("someone" . "someother@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--to-subject
+                        '(:to "foobar@some-news.com"
+                              :subject "\[BLA\]"
+                              :dest "/somewhere")) (list test-msg))))
+  (should-not (let ((test-msg '(:subject "some other dumbass subject"
+                                         :from (("Foobar" . "foobar@some-news.com"))
+                                         :to (("Antoine Dumont" . "some@mail.com")))))
+                (apply (mail-pack-rules-filter-expand-rule--to-subject nil) (list test-msg)))))
+
+
+(ert-deftest test-mail-pack-rules-filter-expand-rule--to-subject ()
+  (should (string= "/default-folder"
+                   (let ((test-msg '(:subject "some other dumbass subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule--default "/default-folder") (list test-msg)))))
+  (should-not (let ((test-msg "something-else-and-still-works"))
+                (apply (mail-pack-rules-filter-expand-rule--to-subject nil) (list test-msg)))))
+
+(ert-deftest test-mail-pack-rules-filter-expand-rule ()
+  (should (string= "/in/the-jungle"
+                   (let ((test-msg '(:subject "[BLA] some hype subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule '(:from "foobar@some-news.com" :dest "/in/the-jungle") "/default") (list test-msg)))))
+
+  (should (string= "/over/the-rainbow"
+                   (let ((test-msg '(:subject "[BLA] some hype subject"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule '(:to "some@mail.com" :dest "/over/the-rainbow") "/default") (list test-msg)))))
+
+  (should (string= "/in/the-sky"
+                   (let ((test-msg '(:subject "[LAB] exciting news!"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule '(:subject "exciting news" :to "some@mail.com" :dest "/in/the-sky") "/default")
+                            (list test-msg)))))
+
+  (should (string= "/under/the-sea"
+                   (let ((test-msg '(:subject "[LAB] exciting news!"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule '(:subject "exciting news" :from "@some-news.com" :dest "/under/the-sea") "/default")
+                            (list test-msg)))))
+
+  (should (string= "/default"
+                   (let ((test-msg '(:subject "[LAB] match not!"
+                                              :from (("Foobar" . "foobar@some-news.com"))
+                                              :to (("Antoine Dumont" . "some@mail.com")))))
+                     (apply (mail-pack-rules-filter-expand-rule nil "/default") (list test-msg))))))
+
+(ert-deftest test-mail-pack-rules-filter-msg ()
+  (should (equal
+           '("/ads" "/ci/travis" "/sfeir" "/job/linkedin" "/job/viadeo" "/hosting/ovh" "/job/ads" "/job/monster" "/swh/devel" "/spam" "/archive")
+           (let ((mail-pack-rules-refiling-rules '((:from "@apple.com"       :dest "/ads" :subject "apple\\|apple developer")
+                                                   (:from "@travis-ci.org"   :dest "/ci/travis")
+                                                   (:from "@sfeir.com"       :dest "/sfeir")
+                                                   (:from "@linkedin.com"    :dest "/job/linkedin")
+                                                   (:from "@viadeo.com"      :dest "/job/viadeo")
+                                                   (:from "@ovh.com"         :dest "/hosting/ovh")
+                                                   (:from "@*monster.com"    :dest "/job/monster")
+                                                   (:from "@octo.com"        :dest "/job/ads")
+                                                   (:to "swh-devel@inria.fr" :dest "/swh/devel")
+                                                   (:subject "\\[blabla\\]" :dest "/spam")))
+                 (msgs '((:subject "Eat some shiny apple"
+                                   :from (("apple" . "apple@apple.com"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "[travis] broken tests"
+                                   :from (("travis" . "travis@travis-ci.org"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "sfeir newsletter"
+                                   :from (("sfeir" . "so@sfeir.com"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "linkedin news on your network"
+                                   :from (("linkedin" . "spam@linkedin.com"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "someone checked out your viadeo profile"
+                                   :from (("viadeo" . "spam@viadeo.com"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "hosting resources stop!"
+                                   :from (("ovh" . "ovh@ovh.com"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "some octo thingy"
+                                   :from (("octo" . "octo@octo.com"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "some ssii wants to check your ass!"
+                                   :from (("monster" . "monster@monster.com"))
+                                   :to (("Antoine Dumont" . "me@me.fr")))
+                         (:subject "checkout that awesome code!"
+                                   :from (("me" . "me@me.fr"))
+                                   :to (("SWH devel" . "swh-devel@inria.fr")))
+                         (:subject "[blabla] some spam you will be delighted with!"
+                                   :to (("me" . "me@me.fr")))
+                         (:subject "no rule"
+                                   :to (("me" . "me@me.fr"))))))
+             (mapcar (-partial #'mail-pack-rules-filter-msg mail-pack-rules-refiling-rules) msgs)))))
+
+(provide 'mail-pack-rules-tests)
+;;; mail-pack-rules-tests.el ends here
---
+++ mail-pack-rules.el
@@ -0,0 +1,146 @@
+;;; mail-pack-rules.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  ardumont
+
+;; Author: ardumont <eniotna.t@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; Define mail-pack rules for refiling mails
+
+;;
+
+;;; Code:
+
+(require 'dash)
+(require 'dash-functional)
+
+(defcustom mail-pack-rules-default-archive-folder "/archive"
+  "Default archive folder if no rules.")
+
+(defcustom mail-pack-rules-refiling-rules nil
+  "Define rules to easily refile emails.
+Possible example:
+  (custom-set-variables '(mail-pack-rules-refiling-rules
+                          '((:from \"@travis-ci.org\"   :dest \"/ci/travis\")
+                            (:from \"@apple.com\"       :dest \"/ads\" :subject \"apple\\\\|apple developer\")
+                            (:from \"@sfeir.com\"       :dest \"/sfeir\")
+                            (:from \"@linkedin.com\"    :dest \"/job/linkedin\")
+                            (:from \"@viadeo.com\"      :dest \"/job/viadeo\")
+                            (:from \"@ovh.com\"         :dest \"/hosting/ovh\")
+                            (:from \"@*monster.com\"    :dest \"/job/monster\")
+                            (:from \"@octo.com\"        :dest \"/job/ads\")
+                            (:to \"swh-devel@inria.fr\" :dest \"/swh/devel\"))))"
+  :type 'list
+  :require 'mail-pack-rules
+  :group 'mail-pack)
+
+(defun mail-pack-rules-filter-expand-rule--from (rule)
+  "Expand from RULE."
+  (lexical-let* ((from-rule (plist-get rule :from))
+                 (dest-rule (plist-get rule :dest)))
+    (lambda (msg)
+      (when (mu4e-message-contact-field-matches msg :from from-rule)
+        dest-rule))))
+
+(defun mail-pack-rules-filter-expand-rule--to (rule)
+  "Expand to RULE."
+  (lexical-let ((to-rule (plist-get rule :to))
+                (dest-rule (plist-get rule :dest)))
+    (lambda (msg) (when (mu4e-message-contact-field-matches msg :to to-rule)
+               dest-rule))))
+
+(defun mail-pack-rules-filter-expand-rule--subject (rule)
+  "Expand to subject RULE."
+  (lexical-let ((subject-rule (plist-get rule :subject))
+                (dest-rule (plist-get rule :dest)))
+    (lambda (msg) (let ((subject (or (mu4e-message-field msg :subject) "")))
+               (when (and subject-rule
+                          (string-match subject-rule subject))
+                 dest-rule)))))
+
+(defun mail-pack-rules-filter-expand-rule--from-subject (rule)
+  "Expand to `'from`' and `'subject`' RULE."
+  (lexical-let ((from-rule    (plist-get rule :from))
+                (subject-rule (plist-get rule :subject))
+                (dest-rule    (plist-get rule :dest)))
+    (lambda (msg) (let ((subject (or (mu4e-message-field msg :subject) "")))
+               (when (and subject-rule
+                          (mu4e-message-contact-field-matches msg :from from-rule)
+                          (string-match subject-rule subject))
+                 dest-rule)))))
+
+(defun mail-pack-rules-filter-expand-rule--to-subject (rule)
+  "Expand to `'to`' and `'subject`' RULE."
+  (lexical-let ((from-rule    (plist-get rule :to))
+                (subject-rule (plist-get rule :subject))
+                (dest-rule    (plist-get rule :dest)))
+    (lambda (msg) (let ((subject (or (mu4e-message-field msg :subject) "")))
+               (when (and subject-rule
+                          (mu4e-message-contact-field-matches msg :to from-rule)
+                          (string-match-p subject-rule subject))
+                 dest-rule)))))
+
+(defun mail-pack-rules-filter-expand-rule--default (default-folder)
+  "Expand DEFAULT-FOLDER rule."
+  (lexical-let ((default default-folder))
+    (lambda (_) default)))
+
+(defun mail-pack-rules-filter-expand-rule (rule default-folder)
+  "Expand rule to a refiling function.
+If RULE is not defined or no rule is found, the DEFAULT-FOLDER is used."
+  (cond ((and (plist-member rule :from)
+              (plist-member rule :subject))
+         (mail-pack-rules-filter-expand-rule--from-subject rule))
+        ((and (plist-member rule :to)
+              (plist-member rule :subject))
+         (mail-pack-rules-filter-expand-rule--to-subject rule))
+        ((plist-member rule :to)
+         (mail-pack-rules-filter-expand-rule--to rule))
+        ((plist-member rule :from)
+         (mail-pack-rules-filter-expand-rule--from rule))
+        ((plist-member rule :subject)
+         (mail-pack-rules-filter-expand-rule--subject rule))
+        (t
+         (mail-pack-rules-filter-expand-rule--default default-folder))))
+
+(defun mail-pack-rules-filter-msg (rules msg &optional default-folder)
+  "Given a list of RULES, filter the MSG.
+Optionally, DEFAULT-FOLDER can be set."
+  (let ((archive-default-folder (if default-folder default-folder "/archive")))
+    (if mail-pack-rules-refiling-rules
+        (->> mail-pack-rules-refiling-rules
+             (mapcar (lambda (rule)
+                       (-> rule
+                           (mail-pack-rules-filter-expand-rule archive-default-folder)
+                           (apply (list msg)))))
+             (-filter (-compose #'not #'null))
+             car)
+      archive-default-folder)))
+
+(custom-set-variables '(mail-pack-rules-refiling-rules
+                        '((:from "@travis-ci.org"   :dest "/ci/travis")
+                          (:from "@apple.com"       :dest "/ads" :subject "apple\\|apple developer")
+                          (:from "@sfeir.com"       :dest "/sfeir")
+                          (:from "@linkedin.com"    :dest "/job/linkedin")
+                          (:from "@viadeo.com"      :dest "/job/viadeo")
+                          (:from "@ovh.com"         :dest "/hosting/ovh")
+                          (:from "@*monster.com"    :dest "/job/monster")
+                          (:from "@octo.com"        :dest "/job/ads")
+                          (:to "swh-devel@inria.fr" :dest "/swh/devel"))))
+
+(provide 'mail-pack-rules)
+;;; mail-pack-rules.el ends here
